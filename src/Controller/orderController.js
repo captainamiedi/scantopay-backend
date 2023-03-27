15c9c4c8acd55368439aa5e091005835ed957ba0
captainamiedi <captainamiedi1@gmail.com>
2023-03-27 13:27:11 +0100

    update

--- src/Controller/orderController.js
+++ src/Controller/orderController.js
@@ -1,4 +1,5 @@
 import { createOrderItemService, getAllOrderForAUserService, getAllOrdersForAStoreService, getOrderItemByIdService } from "../Service/orderService";
+import { createTransactionService } from "../Service/transactionService";
 import { errorResponse, successResponse, successResponseWithData } from "../Utils/response";
 import statusCode from "../Utils/statusCode";
 
@@ -6,7 +7,7 @@ import statusCode from "../Utils/statusCode";
 export default {
     createOrderDetails: async (req, res) => {
         try {
-            const { cart} = req.body
+            const { cart, total, serviceCharge, status, transRef} = req.body
             const cartData = JSON.parse(cart);
             cartData.map(async (item) => {
                 const orderItem = {
@@ -17,6 +18,15 @@ export default {
                 }
                 await createOrderItemService(orderItem)
             })
+            const transactionPayload = {
+                total,
+                charges: serviceCharge, 
+                status, 
+                transRef, 
+                orderId: req.orderData,
+            }
+
+            await createTransactionService(transactionPayload)
 
             return successResponseWithData(res, statusCode.created, 'successfully created!', {orderId: req.orderData})
         } catch (error) {
